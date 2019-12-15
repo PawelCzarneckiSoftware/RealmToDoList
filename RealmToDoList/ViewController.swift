0df84d2ed07c94ec53e6e12a3114a96e7caf2162
Pawel <pawel.czarnecki.software@gmail.com>
2019-12-15 17:21:34 +0100

    Initial Commit

---
+++ RealmToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmToDoList
+//
+//  Created by Pawel Czarnecki on 15/12/2019.
+//  Copyright © 2019 Pawel Czarnecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
